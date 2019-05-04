d3b45a677e78b34ec2ccf7ba3854095d920e192b
Rick Martin <ineedrick@gmail.com>
2019-05-05 08:03:35 +0900

    Updates

---
+++ precache-manifest.0bda7068bf1c4b4bd1d9294c6e2bd71c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42f867ce1799025b333f",
+    "url": "/fugu/static/js/runtime~main.42f867ce.js"
+  },
+  {
+    "revision": "579aed0b7c1e938ca9bc",
+    "url": "/fugu/static/js/main.579aed0b.chunk.js"
+  },
+  {
+    "revision": "10f184e16a79d1974c53",
+    "url": "/fugu/static/js/2.10f184e1.chunk.js"
+  },
+  {
+    "revision": "579aed0b7c1e938ca9bc",
+    "url": "/fugu/static/css/main.5c226279.chunk.css"
+  },
+  {
+    "revision": "10f184e16a79d1974c53",
+    "url": "/fugu/static/css/2.fc85c95f.chunk.css"
+  },
+  {
+    "revision": "056a642028ceab68ab3d490e4ad1929a",
+    "url": "/fugu/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fugu/precache-manifest.a679f110e9a2dbffcf9c19e4479c97c1.js"
+  "/fugu/precache-manifest.0bda7068bf1c4b4bd1d9294c6e2bd71c.js"
 );
 
 workbox.clientsClaim();
