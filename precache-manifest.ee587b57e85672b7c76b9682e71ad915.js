6f1996a30e109593930b121ee2d2c12b9ae33d16
Rick Martin <ineedrick@gmail.com>
2019-05-04 12:02:39 +0900

    Updates

---
+++ precache-manifest.ee587b57e85672b7c76b9682e71ad915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "359e6e9b12c2f7084adb",
+    "url": "/static/js/main.359e6e9b.chunk.js"
+  },
+  {
+    "revision": "290af769eac87f91ee10",
+    "url": "/static/js/2.290af769.chunk.js"
+  },
+  {
+    "revision": "359e6e9b12c2f7084adb",
+    "url": "/static/css/main.5c226279.chunk.css"
+  },
+  {
+    "revision": "290af769eac87f91ee10",
+    "url": "/static/css/2.fc85c95f.chunk.css"
+  },
+  {
+    "revision": "2afa992b6527d14e214cae6637f714aa",
+    "url": "/index.html"
+  }
+];
